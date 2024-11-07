0726a3b1184b4dd64d60110d9099f035431b14e1
Rick Jones <RJones7@wsgc.com>
2024-11-07 10:17:19 -0800

    Backend call to get home page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,64 @@
-import Image from "next/image";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { parseJsonContent } from './utils/parseContentJson'
 
-export default function Home() {
+const baseConfig = {
+  assetsUrl: "https://www.potterybarn.com/",
+  applicationUri: "https://assets.pbimgs.com/pbimgs/rk/images/dp/",
+  headers: {
+    "paw-base-context": "environment=prod-west;application=ecommerce;market=US;concept=PB",
+    "paw-request-time": "2024-11-06T18:23:04.445461Z",
+    "paw-application-uri": "https://www.potterybarn.com/",
+    "paw-asset-uris": "static=https://www.potterybarn.com/.static/;scripts=https://www.potterybarn.com/.static/;images=https://assets.pbimgs.com/pbimgs/rk/images/dp/;ecmImages=https://assets.pbimgs.com/pbimgs/rk/images/dp/;",
+    "paw-active-generations": "CAT=324110524;ECMCS=824451698;ECMHOMEPAGE=824451695;ECMMSG=824451701;ECMPAGES=824451700;ECMPROMOS=824451704;ENDECA=219031803;HOMEPAGE=112013.2;MISC=224110412;MSG=224110412;PROMOS=0;TMPL=224110412",
+    "paw-active-properties": "borderfreeLocalization=US-USD-1;isMobile=0;isSeoBot=0",
+    "paw-context": "generations=CAT ECMCS ECMHOMEPAGE ECMMSG ECMPAGES ECMPROMOS ENDECA HOMEPAGE MISC MSG PROMOS TMPL;properties=borderfreeLocalization isMobile isSeoBot",
+    "paw-service-3": "contract=ecom.dp:1.0;context=3;uris=https://rk-internal-www.potterybarn.com/","paw-context-3":"generations=CAT ECMCS ECMHOMEPAGE ECMMSG ECMPAGES ECMPROMOS ENDECA HOMEPAGE MISC MSG PROMOS TMPL;properties=borderfreeLocalization isMobile isSeoBot",
+    "paw-service-5": "contract=ecom.dp.hostnameOnly:1.0;context=5;uris=https://www.potterybarn.com/",
+    "paw-service-1": "contract=ecom.app.config:1.0;context=1;uris=https://ecommerce-ecom-app-config-all-prod.services.west.wsgc.com/",
+    "paw-context-1": "properties=isMobile isSeoBot",
+    "paw-service-2": "contract=ecom.svc.content.webapp:1.0;context=2 3 5;uris=https://ecommerce-ecom-svc-content-prod.services.west.wsgc.com/",
+    "paw-context-2": "generations=CAT ECMCS ECMHOMEPAGE ECMPAGES ECMPROMOS MSG PROMOS TMPL;properties=isMobile isSeoBot",
+    "paw-services-2": "dp-app-svc=3;dp-app-hostname=5","paw-service-4":"contract=edap.app.config.service:1.0;uris=https://platform-svc-config-prod.services.west.wsgc.com/config-service/v2/feature-config/"
+  },
+  social: {
+      facebook: {
+          appId: "",
+          fbLikeButtonEnabled: false,
+      },
+      pinterest: {
+          pinterestEnabled: false,
+      },
+  },
+};
+
+/**
+ * Fetch the content for the home page.
+ * @returns {Object} the content object
+ */
+async function getData() {
+  const res = await fetch('https://www.potterybarn.com/api/content/v1/content.json?subsystem=ECMHOMEPAGE', { next: { revalidate: 3600 } })
+  
+  if (!res.ok) {
+    throw new Error(`HTTP error! status: ${res.status}`);
+  }
+ 
+  return res.json()
+}
+
+/**
+ * Fetch the home page content and parse it out to form the HTML
+ * to be displayed on page.
+ * @returns {string} the HTML to be displayed
+ */
+export default async function Home() {
+  const response = await getData();
+  const content = parseJsonContent(response.desktop, baseConfig)
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    <main className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-6">Pottery Barn Home</h1>
+      <div className="responsive-content responsive-content-page MFE-HOMEPAGE" data-style="content-page">
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      </div>
+    </main>
+  )
 }
---
+++ app/utils/parseContentJson.tsx
@@ -0,0 +1,387 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const YOUTUBE_VIDEO = "youtube";
+const VIMEO_VIDEO = "vimeo";
+
+let contentVimeoThumbnail = false;
+let dataContent: string | string[] | null;
+
+/**
+ * Parses JSON from ecom-svc-content-webapp into an HTML string
+ * @param { array } contentArray - the array of json objects to parse
+ * @param { Object } config - configuration used in parsing the JSON content
+ * @return { string } the resulting html string
+ */
+export function parseJsonContent(contentArray: any[], config: { assetsUrl: string; applicationUri: string; headers: { "paw-base-context": string; "paw-request-time": string; "paw-application-uri": string; "paw-asset-uris": string; "paw-active-generations": string; "paw-active-properties": string; "paw-context": string; "paw-service-3": string; "paw-context-3": string; "paw-service-5": string; "paw-service-1": string; "paw-context-1": string; "paw-service-2": string; "paw-context-2": string; "paw-services-2": string; "paw-service-4": string; }; social: { facebook: { appId: string; fbLikeButtonEnabled: boolean; }; pinterest: { pinterestEnabled: boolean; }; }; }) {
+    let contentString = "";
+
+    // Apply the parser logic to each element in the data array
+    if (contentArray.length) {
+        contentArray.forEach((content) => {
+            contentString += getParsedContent(content, config);
+        });
+    }
+    return contentString;
+}
+
+/**
+ * Parse the content object and return the html associated with that type.
+ * @param content {Object} the content to be parsed
+ * @param config {Object} the configuration used to process the content
+ * @returns {string} the HTML
+ */
+const getParsedContent = (content: { type: any; data: any; }, config: { assetsUrl: any; applicationUri: any; headers?: { "paw-base-context": string; "paw-request-time": string; "paw-application-uri": string; "paw-asset-uris": string; "paw-active-generations": string; "paw-active-properties": string; "paw-context": string; "paw-service-3": string; "paw-context-3": string; "paw-service-5": string; "paw-service-1": string; "paw-context-1": string; "paw-service-2": string; "paw-context-2": string; "paw-services-2": string; "paw-service-4": string; }; social?: { facebook: { appId: string; fbLikeButtonEnabled: boolean; }; pinterest: { pinterestEnabled: boolean; }; }; ecmMetadata?: any; }) => {
+    const methodName = "getParsedContent";
+    const { type, data } = content;
+
+    switch (type) {
+        case "CONTENT":
+            dataContent = data.CONTENT;
+            // html content strings are added to the output string
+            return _cleanContent(data.CONTENT);
+
+        case "wsgc.contextualappurl":
+        case "wsgc.appurl":
+            // Populate page links based on application url
+            const path = _removePreceedingSlash(data.url);
+            return `${config.applicationUri.concat(path)}`;
+
+        case "wsgc.ecmasseturl":
+            // This provides an absolute url for ecm images etc
+            const root = config.assetsUrl;
+            return root + _removePreceedingSlash(data.url);
+
+        case "ecm.includeAccordionScript":
+            //Empty string returned to remove macro from content, as accordion behavior is handled with event listeners in ecm-content component.
+            return "";
+
+        case "ecm.displayVideo":
+            if (
+                dataContent &&
+                dataContent.length &&
+                dataContent.includes("vimeo-video-thumbnail")
+            ) {
+                contentVimeoThumbnail = true;
+            }
+            dataContent = null;
+            return parseVideoMarkup(content, config);
+
+        case "ecm.includeContent":
+            console.error(
+                `methodName - ${methodName} FTL imports and ecm.includeContent are not supported in MFE. Please use the custom JS component or the custom Vue component instead. (${
+                    (data || {}).contentId
+                })`,
+            );
+            return "";
+
+        case "ecm.vue":
+            return parseVueContent(content, config);
+
+        case "ecmMetadata":
+            config.ecmMetadata = data.ecmMetadata;
+            return "";
+
+        default:
+            // default: log error for invalid type
+            console.error(
+                `No parser available for type ${type}`,
+            );
+            return "";
+    }
+};
+
+const parseVueContent = (content: { type?: any; data: any; }, config: { assetsUrl?: any; applicationUri?: any; headers?: { "paw-base-context": string; "paw-request-time": string; "paw-application-uri": string; "paw-asset-uris": string; "paw-active-generations": string; "paw-active-properties": string; "paw-context": string; "paw-service-3": string; "paw-context-3": string; "paw-service-5": string; "paw-service-1": string; "paw-context-1": string; "paw-service-2": string; "paw-context-2": string; "paw-services-2": string; "paw-service-4": string; } | undefined; social?: { facebook: { appId: string; fbLikeButtonEnabled: boolean; }; pinterest: { pinterestEnabled: boolean; }; } | undefined; ecmMetadata?: any; componentMap?: any; }) => {
+    const methodName = "parseVueContent";
+    const { data } = content;
+
+    let componentProps = [];
+    let contentString = "";
+    if (!data.name) {
+        console.log(
+            `methodName - ${methodName} component missing name property. Please publish component with 'name' property specified.`,
+        );
+        return contentString;
+    }
+    for (const item in data) {
+        if (item !== "name" && item !== "macroType" && item !== "CONTENT") {
+            componentProps.push({
+                attributeName: `${item}`.trim(),
+                attributeValue: `${data[item]}`.trim(),
+            });
+        }
+    }
+    // support re-mapping names from ecm into custom component names, if needed
+    if (config?.componentMap?.[data.name]) {
+        data.name = config?.componentMap?.[data.name];
+    }
+    // support re-mapping names from ecm into custom component names, if needed
+    if (config?.componentMap?.[data.name]) {
+        data.name = config?.componentMap?.[data.name];
+    }
+    componentProps = JSON.stringify(componentProps);
+
+    contentString = `<div class="vue-reference-component ${data.name}" data-name="${data.name}" data-props=${componentProps}></div>`;
+
+    return contentString;
+};
+
+/**
+ * Parse video content.
+ * @param content {Object} the content to be parsed
+ * @param config {Object} the configuration used to process the content
+ * @returns {string} HTML
+ */
+const parseVideoMarkup = (content: { type?: any; data: any; }, config: object) => {
+    // This renders an embedded video, optionally with hooks to wrap it in a modal later
+    // destinationURL is a property on the data object that will be used for embdedd video clickthroughs
+    const { data } = content;
+    // inverts the showControls flag
+    const controlsParam = data && data.showControls ? `&amp;controls=0` : "";
+    // playlist attribute required for youtube to loop single video
+    const playlistParam =
+        data && data.loopVideo ? `&amp;playlist=${data.videoId}` : "";
+    const youtubeIframeUrl = `https://www.youtube.com/embed/${
+        data.videoId
+    }?enablejsapi=1&amp;version=3&amp;rel=0&amp;wmode=transparent&amp;modestbranding=1&amp;autoplay=${
+        data.autoplay ? "1" : "0"
+    }&amp;loop=${
+        data.loopVideo ? "1" : "0"
+    }&amp;mute=1${controlsParam}${playlistParam}`;
+
+    const youtubeIframeThumbnail = `https://img.youtube.com/vi/${data.videoId}/sddefault.jpg`;
+    const embedUrl = `https://www.youtube.com/embed/${data.videoId}?wmode=transparent&autohide=1&autoplay=${data.autoplay}&iv_load_policy=3&fs=1&cc_load_policy=0&rel=0&showsearch=0&enablejsapi=1&version=3&webkitAllowFullScreen=webkitAllowFullScreen&mozallowfullscreen=mozallowfullscreen&allowFullScreen=allowFullScreen&theme=light&origin=${config.applicationUri}&widgetid=2`;
+
+    if (!Object.hasOwn(config, "itemListElement")) {
+        config.itemListElement = [];
+    }
+
+    config.itemListElement.push({
+        "@type": "VideoObject",
+        name: data.name,
+        url: config.applicationUri,
+        description: data.description,
+        thumbnailUrl: youtubeIframeThumbnail,
+        uploadDate: data?.uploadDate ?? `${new Date()}`,
+        embedUrl: embedUrl,
+    });
+
+    let vimeoThumbnailUrl = "";
+
+    if (Array.isArray(data.CONTENT) && data.CONTENT.length) {
+        data.CONTENT.forEach((videoContent) => {
+            if (videoContent.type === "wsgc.ecmasseturl") {
+                vimeoThumbnailUrl =
+                    config.assetsUrl +
+                    _removePreceedingSlash(videoContent.data.url);
+            }
+        });
+    }
+
+    const vimeoIframeUrl = `https://player.vimeo.com/video/${data.videoId}${
+        data.videoId.includes("?") ? "&amp;" : "?"
+    }autoplay=${
+        data.autoplay ? "true" : "false"
+    }&amp;byline=0&amp;title=0&amp;loop=${
+        data.loopVideo ? "true" : "false"
+    }&amp;autopause=false&amp;muted=1${controlsParam}`;
+
+    let socialIconsContent = "";
+    let videoModalContentString = "";
+    let videoContentString = "";
+    let url = "";
+    let videoSchemaObject;
+
+    if (data.videoProvider && data.videoProvider === YOUTUBE_VIDEO) {
+        url = youtubeIframeUrl;
+    } else if (data.videoProvider === VIMEO_VIDEO) {
+        url = vimeoIframeUrl;
+    } else {
+        // if videoProvider prop isn't present, fallback to videoId
+        if (data.videoId.match(/[a-zA-Z]/)) {
+            // If the video ID is youtube formatted, use a youtube iframe url
+            url = youtubeIframeUrl;
+        } else {
+            // Otherwise, use a vimeo iframe url
+            url = vimeoIframeUrl;
+        }
+    }
+
+    if (data.CONTENT && data.CONTENT.length && !contentVimeoThumbnail) {
+        // handle content array, for nested mixed content
+        if (data.CONTENT.forEach) {
+            data.CONTENT.forEach((videoContent: { type: any; data: any; }) => {
+                videoModalContentString += getParsedContent(
+                    videoContent,
+                    config,
+                );
+            });
+        } else {
+            // handle content string
+            videoContentString += _cleanContent(data.CONTENT);
+        }
+    }
+
+    let componentProps = [];
+
+    for (const item in data) {
+        if (
+            item !== "destinationUrl" &&
+            item !== "name" &&
+            item !== "description" &&
+            item !== "macroType" &&
+            item !== "CONTENT"
+        ) {
+            componentProps.push({
+                attributeName: item,
+                attributeValue: data[item],
+            });
+        }
+    }
+
+    const finalVueMarkup = `<div class="vue-video-component" data-name="VideoContent" data-props=${JSON.stringify(
+        componentProps,
+    )}> </div>`;
+
+    const videoMarkup =
+        data.videoProvider === VIMEO_VIDEO
+            ? `<div class="responsive-video-wrapper"> 
+            <vimeo-video 
+                thumbnail="${vimeoThumbnailUrl}"
+                url="${url}" 
+                ${data.destinationUrl ? `destination-url=${config.applicationUri.concat(_removePreceedingSlash(data.destinationUrl))}` : ""}
+                ${data.showControls ? "" : "controls"}
+                ${data.autoplay ? "autoplay" : ""}
+                ${data.loopVideo ? "loop" : ""}
+                ${data.CONTENT && data.CONTENT.length && !contentVimeoThumbnail ? "is-video-modal" : ""}
+                ${data.width ? `width=${data.width}` : ""} 
+                ${data.height ? `height=${data.height}` : ""}>
+                
+            </vimeo-video>
+            ${
+                videoContentString
+                    ? `<div class="videoContent">${videoContentString}</div>`
+                    : ""
+            }
+        </div>`
+            : `<div class="responsive-video-wrapper">
+          <iframe class="responsiveVideo" src="${url}" ${
+                  data.width ? `width=${data.width}` : ""
+              } ${
+                  data.height ? `height=${data.height}` : ""
+              } webkitallowfullscreen="webkitallowfullscreen" mozallowfullscreen="mozallowfullscreen" allowfullscreen="allowfullscreen"></iframe>
+          ${
+              videoContentString
+                  ? `<div class="videoContent">${videoContentString}</div>`
+                  : ""
+          }
+        </div>`;
+
+    videoSchemaObject = {
+        "@context": "https://schema.org",
+        "@type": "ItemList",
+        itemListElement: config.itemListElement,
+    };
+
+    videoSchemaObject["itemListElement"].forEach((video, index) => {
+        video.position = index + 1;
+    });
+
+    config.videoSchemaObject = videoSchemaObject;
+
+    if (!data.isOverlay && config.lazyLoadVideosEnabled) {
+        return finalVueMarkup;
+    } else if (!data.isOverlay) {
+        return videoMarkup;
+    }
+
+    const overlayLinkMatch = videoModalContentString.match(
+        /href="#id([0-9a-zA-Z]+)"/i,
+    );
+    const overlayLink = overlayLinkMatch ? `id${overlayLinkMatch[1]}` : "";
+    data.overlayLinkId = overlayLink || data.overlayLinkId;
+
+    if (data.displaySocialIcons) {
+        const thumbnailImageSrc = _extractImgSrc(videoModalContentString);
+        const fbLikeWidget = `<li class="facebook">
+                              <div class="fb-like" 
+                                  data-href="${url}" 
+                                  data-layout="button_count" 
+                                  data-send="false" 
+                                  data-width="90" 
+                                  data-action="like" 
+                                  data-show-faces="true" 
+                                  data-colorscheme="light" 
+                                  data-font="verdana" 
+                                  width="90"></div>
+                             </li>`;
+        const pinterestWidget = `<li class="pinterest">
+                                  <a href="http://pinterest.com/pin/create/button/" 
+                                      class="pin-it-button" 
+                                      data-pin-do="buttonPin" 
+                                      data-pin-url="${url}" 
+                                      data-pin-media="${thumbnailImageSrc}" 
+                                      data-pin-description="${data.description}" 
+                                      data-pin-count="beside">
+                                  </a>
+                                </li>`;
+
+        socialIconsContent = `<div>
+                                <ul class="social-icons">
+                                  ${
+                                      config.social.facebook.fbLikeButtonEnabled
+                                          ? fbLikeWidget
+                                          : ""
+                                  }
+                                  ${
+                                      config.social.pinterest.pinterestEnabled
+                                          ? pinterestWidget
+                                          : ""
+                                  }
+                                </ul>
+                              </div>`;
+    }
+
+    contentVimeoThumbnail = false;
+
+    return `<div class="videoThumbnail modal">
+                  <div class="thumbnailContent">
+                    ${videoModalContentString}
+                  </div>
+                  <div class="modalHook" id="${data.overlayLinkId}">
+                    ${videoMarkup}
+                    ${socialIconsContent}
+                  </div>
+                </div>`;
+};
+
+/**
+ * Utility to lift image src attributes for legacy video thumbnails
+ * @param {string} content - to process
+ * @returns {string} the video attributes
+ */
+const _extractImgSrc = (content: string) => {
+    const imgSources = content.match(/src\s*=\s*"(.+?)"/gm);
+    return (
+        imgSources &&
+        imgSources[0] &&
+        imgSources[0].substring(5, imgSources[0].length - 1)
+    );
+};
+
+// Utility to ensure URI has no initial slash
+const _removePreceedingSlash = (path: string) => {
+    return path.startsWith("/") ? path.substring(1) : path;
+};
+
+/** 
+ * Utility to clean up content string
+ * @param content {string} the string gto process
+ * @return {string} Update specific tags to allow correct rendering
+ */
+const _cleanContent = (content: string) =>
+    content
+        .replace(/<script/gi, "<noscript  data-tag-name='script'")
+        .replace(/<\/script/gi, "</noscript")
+        .replace(/<body/gi, "<div")
+        .replace(/<\/body/gi, "</div")
+        .replace(/<style/gi, "<noscript data-tag-name='style'")
+        .replace(/<\/style/gi, "</noscript");
